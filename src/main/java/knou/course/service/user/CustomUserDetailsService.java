941b93c156cfe8512d08c8642247053f9ce1df42
YunByungil <dbsquddlfz@naver.com>
2024-07-26 18:38:06 +0900

    feat: 로그인 기능 구현

---
+++ src/main/java/knou/course/service/user/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package knou.course.service.user;
+
+import knou.course.domain.user.User;
+import knou.course.domain.user.UserRepository;
+import knou.course.dto.user.request.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return new CustomUserDetails(user);
+    }
+}
