8b3d26dfa36209892bde3eaae5f4f2e29b7f5992
YunByungil <dbsquddlfz@naver.com>
2024-07-29 20:13:45 +0900

    test: 회원가입 로직에 이메일 인증 여부 및 중복 검사 로직 테스트

--- src/test/java/knou/course/service/user/UserServiceTest.java
+++ src/test/java/knou/course/service/user/UserServiceTest.java
@@ -1,11 +1,15 @@
 package knou.course.service.user;
 
+import knou.course.domain.mail.MailHistory;
+import knou.course.domain.mail.MailHistoryRepository;
 import knou.course.domain.user.Role;
 import knou.course.domain.user.Status;
 import knou.course.domain.user.User;
 import knou.course.domain.user.UserRepository;
 import knou.course.dto.user.request.EmailRequest;
+import knou.course.dto.user.request.UserCreateRequest;
 import knou.course.dto.user.request.UsernameRequest;
+import knou.course.dto.user.response.UserResponse;
 import knou.course.exception.AppException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -26,9 +30,13 @@ class UserServiceTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private MailHistoryRepository mailHistoryRepository;
+
     @AfterEach
     void tearDown() {
         userRepository.deleteAllInBatch();
+        mailHistoryRepository.deleteAllInBatch();
     }
 
     @DisplayName("아이디 중복검사를 성공한다.")
@@ -91,6 +99,56 @@ class UserServiceTest {
         assertThatThrownBy(() -> userService.checkEmailDuplication(request.getEmail()))
                 .isInstanceOf(AppException.class)
                 .hasMessage("이미 존재하는 유저입니다.");
+    }
+
+    @DisplayName("회원가입을 완료한다.")
+    @Test
+    void createUser() {
+        // given
+        MailHistory mailHistory = MailHistory.builder()
+                .code(123456)
+                .email("email@knou.ac.kr")
+                .confirm(true)
+                .build();
+        mailHistoryRepository.save(mailHistory);
+
+        UserCreateRequest request = UserCreateRequest.builder()
+                .username("username")
+                .password("password")
+                .email("email@knou.ac.kr")
+                .build();
+
+        // when
+        UserResponse userResponse = userService.singUp(request);
+
+        // then
+        assertThat(userResponse.getId()).isNotNull();
+        assertThat(userResponse)
+                .extracting("username", "email")
+                .containsExactlyInAnyOrder("username", "email@knou.ac.kr");
+    }
+
+    @DisplayName("회원가입을 진행할 때 이메일 인증을 안 하면 예외가 발생한다.")
+    @Test
+    void createUserWithoutEmailAuthentication() {
+        // given
+        MailHistory mailHistory = MailHistory.builder()
+                .code(123456)
+                .email("email@knou.ac.kr")
+                .confirm(false)
+                .build();
+        mailHistoryRepository.save(mailHistory);
+
+        UserCreateRequest request = UserCreateRequest.builder()
+                .username("username")
+                .password("password")
+                .email("email@knou.ac.kr")
+                .build();
+
+        // when // then
+        assertThatThrownBy(() -> userService.singUp(request))
+                .isInstanceOf(AppException.class)
+                .hasMessage("이메일 인증이 필요합니다.");
     }
 
     private User createUser(final String username, final String password, final String email) {
