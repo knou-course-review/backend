e4cc4aa803b201ac5edb2d5058fbd77bc6b9ffc3
YunByungil <dbsquddlfz@naver.com>
2024-08-08 20:39:18 +0900

    feat: 강의 검색(학과명, 교수명, 강의명) 기능 구현

--- src/main/java/knou/course/domain/course/CourseRepository.java
+++ src/main/java/knou/course/domain/course/CourseRepository.java
@@ -1,6 +1,38 @@
 package knou.course.domain.course;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    @Query(value = "SELECT c " +
+            "FROM Course c " +
+            "WHERE c.professorId IN ( " +
+            "SELECT p.id " +
+            "FROM Professor p " +
+            "WHERE p.professorName LIKE :name)")
+    Page<Course> customFindByProfessorId(@Param("name") String name, Pageable pageable);
+
+    @Query(value = "SELECT c " +
+            "FROM Course c " +
+            "WHERE c.departmentId IN ( " +
+            "SELECT d.id " +
+            "FROM Department d " +
+            "WHERE d.departmentName LIKE :name)")
+    Page<Course> customFindByDepartmentId(@Param("name") String name, Pageable pageable);
+
+    @Query(value = "SELECT c " +
+            "FROM Course c " +
+            "WHERE c.id IN ( " +
+            "SELECT c.id " +
+            "FROM Course c " +
+            "WHERE c.courseName LIKE :name)")
+    Page<Course> customFindByCourseId(@Param("name") String name, Pageable pageable);
+
 }
