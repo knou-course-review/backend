6e94f7bc5a3fea0321e6bd1b597a8c3e21e0b6d0
YunByungil <dbsquddlfz@naver.com>
2024-08-13 15:12:43 +0900

    test: ReviewLike 리뷰 좋아요 기능 테스트 코드 작성

---
+++ src/test/java/knou/course/controller/like/ReviewLikeControllerTest.java
@@ -0,0 +1,59 @@
+package knou.course.controller.like;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import knou.course.service.like.ReviewLikeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = ReviewLikeController.class)
+class ReviewLikeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private ReviewLikeService reviewLikeService;
+
+    @BeforeEach
+    void setUp() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(new UsernamePasswordAuthenticationToken("1", "", List.of()));
+    }
+
+    @DisplayName("리뷰 좋아요")
+    @Test
+    void createReviewLike() throws Exception {
+        // given
+
+        // when // then
+        mockMvc.perform(
+                        post("/api/v1/like/{reviewId}", 1L).with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.message").value("OK"));
+    }
+}
