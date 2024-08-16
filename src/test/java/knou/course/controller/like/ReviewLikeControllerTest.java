5b3586732f1a0511f5eeabdf85486f15d200c3c1
YunByungil <dbsquddlfz@naver.com>
2024-08-16 18:54:54 +0900

    test: 리뷰 목록에 대한 좋아요 개수, 좋아요 여부 조회 기능 테스트 코드 작성

--- src/test/java/knou/course/controller/like/ReviewLikeControllerTest.java
+++ src/test/java/knou/course/controller/like/ReviewLikeControllerTest.java
@@ -1,10 +1,12 @@
 package knou.course.controller.like;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import knou.course.dto.like.response.ReviewLikeStatusResponse;
 import knou.course.service.like.ReviewLikeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,8 +20,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -69,6 +70,23 @@ class ReviewLikeControllerTest {
         mockMvc.perform(
                         delete("/api/v1/like/{reviewId}", reviewId).with(csrf())
                                 .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("리뷰 좋아요 개수와 여부")
+    @Test
+    void getReviewLikes() throws Exception {
+        // given
+        List<ReviewLikeStatusResponse> result = List.of();
+
+        BDDMockito.given(reviewLikeService.getLikeStatusByReviewIds(List.of(), 1L)).willReturn(result);
+        // when // then
+        mockMvc.perform(
+                        get("/api/v1/likes").with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .param("reviewIds", "1", "2", "3")
                 )
                 .andDo(print())
                 .andExpect(status().isOk());
--- src/test/java/knou/course/service/like/ReviewLikeServiceTest.java
+++ src/test/java/knou/course/service/like/ReviewLikeServiceTest.java
@@ -3,6 +3,7 @@ package knou.course.service.like;
 import knou.course.domain.like.ReviewLike;
 import knou.course.domain.like.ReviewLikeRepository;
 import knou.course.dto.like.response.ReviewLikeResponse;
+import knou.course.dto.like.response.ReviewLikeStatusResponse;
 import knou.course.exception.AppException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
@@ -90,6 +92,31 @@ class ReviewLikeServiceTest {
         assertThatThrownBy(() -> reviewLikeService.deleteReviewLike(reviewId, userId))
                 .isInstanceOf(AppException.class)
                 .hasMessage("좋아요가 존재하지 않습니다.");
+    }
+
+    @DisplayName("리뷰에 대한 좋아요 개수, 좋아요 여부를 조회한다.")
+    @Test
+    void getLikeStatusByReviewIds() {
+        // given
+        final Long userId = 1L;
+        ReviewLike reviewLike1 = createReviewLike(1L, 1L);
+        ReviewLike reviewLike2 = createReviewLike(2L, 2L);
+        ReviewLike reviewLike3 = createReviewLike(3L, 3L);
+        ReviewLike reviewLike4 = createReviewLike(1L, 4L);
+        reviewLikeRepository.saveAll(List.of(reviewLike1, reviewLike2, reviewLike3, reviewLike4));
+
+        // when
+        List<ReviewLikeStatusResponse> reviewLikeStatusResponse = reviewLikeService.getLikeStatusByReviewIds(List.of(1L, 2L, 3L), userId);
+
+        // then
+        assertThat(reviewLikeStatusResponse).hasSize(3)
+                .extracting("reviewId", "likeCount", "isLike")
+                .containsExactlyInAnyOrder(
+                        tuple(1L, 2L, true),
+                        tuple(2L, 1L, false),
+                        tuple(3L, 1L, false)
+                );
+
     }
     
     private ReviewLike createReviewLike(final Long reviewId, final Long userId) {
